9d1c3ecfa6bfa4719975a9e783920298cd2a7e81
Mohammed-Hamada <mohammed3rbio@gmail.com>
2023-05-30 01:31:32 +0300

    configuer the dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 import { config } from 'dotenv';
 import { Dialect } from 'sequelize';
+
+import { Otp, User } from './models';
 import { UserModule } from './modules/user/user.module';
-import { AuthService } from './modules/auth/auth.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { User } from './models';
+import { OtpModule } from './modules/otp/otp.module';
+import { EmailModule } from './modules/email/email.module';
 
 config();
 
@@ -17,7 +21,7 @@ const sequelizeConfig = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_DEVELOPMENT,
-  models: [User],
+  models: [User, Otp],
   synchronize: true,
   define: {
     defaultScope: {
@@ -32,8 +36,19 @@ const sequelizeConfig = {
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRoot(sequelizeConfig),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILER_HOST,
+        auth: {
+          user: process.env.MAILER_USERNAME,
+          pass: process.env.MAILER_PASSWORD,
+        },
+      },
+    }),
     UserModule,
     AuthModule,
+    OtpModule,
+    EmailModule,
   ],
 })
 export class AppModule {}
