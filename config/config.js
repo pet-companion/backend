e81a2166931edd2795b0205bc3eb03a488636ed3
Mohammed-Hamada <mohammed3rbio@gmail.com>
2023-05-31 01:36:20 +0300

    trying to fix deployment error

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const { config } = require('dotenv');
+
 config();
 
 module.exports = {
@@ -9,5 +10,20 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
