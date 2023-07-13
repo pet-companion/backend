356ac94c506ed2b84cb824a68dfd7cb18f12fb5b
Mohammed-Hamada <mohammed3rbio@gmail.com>
2023-07-13 11:16:23 +0300

    Add swagger spec and use swagger plugin to automate the process of defining the API

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -11,26 +12,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
   app.setGlobalPrefix('api/v1');
 
+  const config = new DocumentBuilder()
+    .setTitle('Pets example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
-
-  // const server = app.getHttpServer();
-  // const router = server._events.request._router;
-
-  // const availableRoutes: [] = router.stack
-  //   .map((layer) => {
-  //     if (layer.route) {
-  //       return {
-  //         route: {
-  //           path: layer.route?.path,
-  //           method: layer.route?.stack[0].method,
-  //         },
-  //       };
-  //     }
-  //   })
-  //   .filter((item) => item !== undefined);
-  // console.log(availableRoutes);
 }
 bootstrap();
